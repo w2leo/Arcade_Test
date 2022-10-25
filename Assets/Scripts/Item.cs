1ce5cd8f3527336d75b3ff16ca93b0ef347a0fca
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-25 15:41:27 +0300

    Added prefabs
    Added gem, particle, font.
    Added UI text
    Tested compare speed
    CompareTag vs GetComponent vs TryGetComponent.
    For best time - CompareTag
    For minimal errors (with string CompareTag) - use TryGetComponent<>

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem particleTake;
 
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        CompareSpeedTest.TEST_COMPARE<Player, Item>(other.transform, "Player", "MainCamera", 100000);
+        
+        //if (other.TryGetComponent<Player>(out Player player))
+        //{
+        //    particleTake.transform.position = transform.position;
+        //    particleTake.Play();
+        //    gameObject.SetActive(false);
+        //}
+
+    }
+
+ 
 }
