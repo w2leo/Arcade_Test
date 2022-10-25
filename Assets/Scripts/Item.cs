2cb76da9d5ae5bc7b7010a50511ecbdcb12e68f0
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-25 16:21:34 +0300

    update Item.cs
    player collision detection.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     [SerializeField] private ParticleSystem particleTake;
-
     
     private void OnTriggerEnter(Collider other)
     {
-        CompareSpeedTest.TEST_COMPARE<Player, Item>(other.transform, "Player", "MainCamera", 100000);
+        //CompareSpeedTest.TEST_COMPARE<Player, Item>(other.transform, "Player", "MainCamera", 100000);
         
-        //if (other.TryGetComponent<Player>(out Player player))
-        //{
-        //    particleTake.transform.position = transform.position;
-        //    particleTake.Play();
-        //    gameObject.SetActive(false);
-        //}
+        //Tested compare speed
+        //CompareTag vs GetComponent vs TryGetComponent.
+        //For best time - CompareTag
+        //For minimal errors(with string in CompareTag) - better to use TryGetComponent<>
+
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            particleTake.transform.position = transform.position;
+            particleTake.Play();
+            gameObject.SetActive(false);
+        }
 
     }
 
