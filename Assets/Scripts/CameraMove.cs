539f6a014d421be8842c68d21c30c85b61ca8510
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-26 12:09:09 +0300

    Update CameraMove
    without Lerp in movement, because players moves with Lerp.

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     [SerializeField] private Player player;
-    [SerializeField] private float moveSpeed;
     [SerializeField] private float cameraHeight = 10f;
     [SerializeField] private float zCameraOffset = -5f;
 
-    private Vector3 cameraOffset => new Vector3(0f, cameraHeight, zCameraOffset);
+    private Vector3 CameraOffset => new Vector3(0f, cameraHeight, zCameraOffset);
 
-    private Vector3 CameraPosition => player.transform.position + cameraOffset;
+    private Vector3 CameraPosition => player.transform.position + CameraOffset;
 
     public void SetCameraToPlayer()
     {
@@ -21,6 +20,6 @@ public class CameraMove : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, CameraPosition, moveSpeed * Time.deltaTime);
+        SetCameraToPlayer();
     }
 }
